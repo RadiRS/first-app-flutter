8a77a4a72912fbc4fdb3747d6057dabd613c8db4
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-02-23 20:15:55 +0700

    Collors & Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,21 @@ void main() {
       appBar: AppBar(
         title: Text('Home Screen'),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text('Hello World'),
+        child: Text('Hello World',
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.grey[600],
+                fontFamily: 'Lato')),
       ),
       floatingActionButton: FloatingActionButton(
         child: Text('Click'),
+        onPressed: () => print('Press'),
+        backgroundColor: Colors.redAccent,
       ),
     ),
   ));
