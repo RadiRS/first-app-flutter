a717f73d86e9a9a75c50e761ae2703fd1e3d54b0
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-04-21 06:47:29 +0700

    Tested get time from worldtimeapi

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -11,17 +11,27 @@ class _LoadingState extends State<Loading> {
   @override
   void initState() {
     super.initState();
-    getDate();
+    getTime();
   }
 
-  void getDate() async {
-    Response response =
-        await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+  void getTime() async {
+    Response response = await get(
+        Uri.parse('https://worldtimeapi.org/api/timezone/Asia/Jakarta'));
 
     Map data = jsonDecode(response.body);
 
     print(data);
-    print(data['title']);
+
+    // get propertiest from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    // print('$datetime $offset');
+
+    // create datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
