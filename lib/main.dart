b41423716508af9af15b331738308d14f61737c9
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-04-20 20:51:32 +0700

    Added Card Widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,29 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: 'Brian Lai', text: 'Ab at similique consequuntur vel.'),
   ];
 
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+            ),
+            SizedBox(height: 6),
+            Text(
+              quote.author,
+              style: TextStyle(fontSize: 14, color: Colors.grey[800]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,9 +48,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes
-            .map((item) => Text('${item.text} - ${item.author}'))
-            .toList(),
+        children: quotes.map((item) => quoteTemplate(item)).toList(),
       ),
     );
   }
