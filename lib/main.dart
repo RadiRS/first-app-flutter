b0fbd1545b81490b371f061982774615fde9b6cb
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-04-21 05:04:16 +0700

    Added screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/pages/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text('Initial Project'),
+    home: Home(),
   ));
 }
