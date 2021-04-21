ec3615e33d0ef8ce9930335fead112e8dc7c675c
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-04-21 08:14:11 +0700

    Added loader at loading screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:first_app/services/world_time.dart';
 
 class Loading extends StatefulWidget {
@@ -32,9 +33,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: EdgeInsets.all(50),
-      child: Text('loading...'),
-    ));
+        backgroundColor: Colors.blue[900],
+        body: Center(
+          child: SpinKitFadingCube(
+            color: Colors.white,
+            size: 50.0,
+          ),
+        ));
   }
 }
