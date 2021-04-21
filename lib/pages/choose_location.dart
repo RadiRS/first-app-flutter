51026ecb4dc7b9ed5ca470207c7ddd4edb267e4b
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-04-21 08:49:36 +0700

    Added ListView builder to render list of data

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/services/world_time.dart';
 import 'package:flutter/material.dart';
 
 class ChooseLocation extends StatefulWidget {
@@ -6,28 +7,20 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  int counter = 0;
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
 
   @override
   void initState() {
     super.initState();
-    getDate();
-  }
-
-  void getDate() async {
-    //  simulate network request
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      // similiar with setTimeOut
-      return "radi";
-    });
-
-    //  simulate network request to get data
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      // similiar with setTimeOut
-      return "software engineer";
-    });
-
-    print('$username is $bio');
   }
 
   @override
@@ -40,13 +33,23 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: ElevatedButton(
-        onPressed: () {
-          setState(() {
-            counter += 1;
-          });
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+            child: Card(
+              child: ListTile(
+                onTap: () {},
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage:
+                      AssetImage('assets/${locations[index].flag}'),
+                ),
+              ),
+            ),
+          );
         },
-        child: Text('counter is $counter'),
       ),
     );
   }
