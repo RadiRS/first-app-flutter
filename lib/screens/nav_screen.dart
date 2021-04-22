43e3a51fbb9b14063b74d0a0847e1959a77da976
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-04-22 13:49:04 +0700

    Added miniplayer at nav_screen

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_youtube_ui/data.dart';
+import 'package:miniplayer/miniplayer.dart';
 import 'home_screen.dart';
 
 final selectedVideoProvider = StateProvider<Video?>((ref) => null);
@@ -11,6 +12,8 @@ class NavScreen extends StatefulWidget {
 }
 
 class _NavScreenState extends State<NavScreen> {
+  static const double _playerMinHeight = 60.0;
+
   int _selectedIndex = 0;
 
   final _screens = [
@@ -34,7 +37,91 @@ class _NavScreenState extends State<NavScreen> {
               .map((i, screen) => MapEntry(
                   i, Offstage(offstage: _selectedIndex != i, child: screen)))
               .values
-              .toList(),
+              .toList()
+                ..add(
+                  Offstage(
+                    offstage: selectedVideo == null,
+                    child: Miniplayer(
+                      minHeight: _playerMinHeight,
+                      maxHeight: MediaQuery.of(context).size.height,
+                      builder: (height, percentage) {
+                        if (selectedVideo == null) return SizedBox.shrink();
+
+                        return Container(
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Image.network(
+                                    selectedVideo.thumbnailUrl,
+                                    height: _playerMinHeight - 4.0,
+                                    width: 120,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Flexible(
+                                            child: Text(
+                                              selectedVideo.title,
+                                              overflow: TextOverflow.ellipsis,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .caption!
+                                                  .copyWith(
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.w500,
+                                                  ),
+                                            ),
+                                          ),
+                                          Flexible(
+                                            child: Text(
+                                              selectedVideo.author.username,
+                                              overflow: TextOverflow.clip,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .caption!
+                                                  .copyWith(
+                                                    fontWeight: FontWeight.w500,
+                                                  ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  IconButton(
+                                    icon: Icon(Icons.play_arrow),
+                                    onPressed: () {},
+                                  ),
+                                  IconButton(
+                                    icon: Icon(Icons.close),
+                                    onPressed: () {
+                                      context
+                                          .read(selectedVideoProvider)
+                                          .state = null;
+                                    },
+                                  )
+                                ],
+                              ),
+                              LinearProgressIndicator(
+                                value: 0.5,
+                                valueColor:
+                                    AlwaysStoppedAnimation<Color>(Colors.red),
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
         );
       }),
       bottomNavigationBar: BottomNavigationBar(
