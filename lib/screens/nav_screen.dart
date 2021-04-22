db705956c8d4a43ca131557e1c674172f534ebdd
Radi Rusadi <radi.susanto@modalrakyat.id>
2021-04-22 09:31:29 +0700

    Added nav screen ui

---
+++ lib/screens/nav_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class NavScreen extends StatefulWidget {
+  @override
+  _NavScreenState createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: (i) => setState(() => _selectedIndex = i),
+        selectedFontSize: 10.0,
+        unselectedFontSize: 10.0,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              activeIcon: Icon(Icons.home),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.explore_outlined),
+              activeIcon: Icon(Icons.explore),
+              label: 'Explore'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle_outline),
+              activeIcon: Icon(Icons.add_circle),
+              label: 'Add'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.subscriptions_outlined),
+              activeIcon: Icon(Icons.subscriptions),
+              label: 'Subscriptions'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.video_library_outlined),
+              activeIcon: Icon(Icons.video_library),
+              label: 'Library'),
+        ],
+      ),
+    );
+  }
+}
